6def77ceaad67d8ee45735817fad41843b35d21b
Arun Kumar <arunraj963686@gmail.com>
2023-11-30 23:24:32 +0530

    Set zeros solution added

--- Arrays/SetZeros.java
+++ Arrays/SetZeros.java
@@ -14,45 +14,50 @@ class SetZeros {
 		
 	}
     //brute force approach
-    public static void setZeroes(int[][] matrix) {
-    	for(int r=0;r<matrix.length;r++){
-             for(int c=0;c<matrix[0].length;c++){
-                 if(matrix[r][c]==0){
-                     for(int i=0;i<matrix.length;i++)
-                         if(matrix[i][c]!=0) matrix[i][c]=-1;
-                     for(int j=0;j<matrix[0].length;j++)
-                         if(matrix[r][j]!=0) matrix[r][j]=-1;
-                 }
-             }
-         }
-
-         for(int r=0;r<matrix.length;r++){
-             for(int c=0;c<matrix[0].length;c++){
-                 if(matrix[r][c]==-1) matrix[r][c]=0;
-             }
-         }
-    }
+//    public static void setZeroes(int[][] matrix) {
+//    	for(int r=0;r<matrix.length;r++){
+//             for(int c=0;c<matrix[0].length;c++){
+//                 if(matrix[r][c]==0){
+//                     for(int i=0;i<matrix.length;i++)
+//                         if(matrix[i][c]!=0) matrix[i][c]=-1;
+//                     for(int j=0;j<matrix[0].length;j++)
+//                         if(matrix[r][j]!=0) matrix[r][j]=-1;
+//                 }
+//             }
+//         }
+//
+//         for(int r=0;r<matrix.length;r++){
+//             for(int c=0;c<matrix[0].length;c++){
+//                 if(matrix[r][c]==-1) matrix[r][c]=0;
+//             }
+//         }
+//    }
 
 //    //better approach
-//    public void setZeroes(int[][] matrix) {
-//
-//        int r[]=new int[matix.length];
-//        int c[]=new int[matix[0].length]; 
-//        for(int i=0;i<matrix.length;i++){
-//            for(int j=0;j<matrix[0].length;j++){
-//                if(matrix[i][j]==0){
-//                    r[i] = 0;
-//                    c[j] = 0;
-//                }
-//            }
-//        }
-//
-//        for(int i=0;i<matrix.length;i++){
-//            for(int j=0;j<matrix[0].length;j++){
-//                    if(r[i]==0 || c[i]==0){
-//                        matrix[i][j]
-//                    }
-//    }
+    public static void setZeroes(int[][] matrix) {
+
+        int r[]=new int[matrix.length];
+        int c[]=new int[matrix[0].length]; 
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    r[i] = 0;
+                    c[j] = 0;
+                }
+            }
+        }
+
+        for(int i=0;i<matrix.length;i++){
+        	for(int j=0;j<matrix[0].length;j++){
+            if(r[i]==0 || c[j]==0){
+	            j=0;
+	        	while(j<matrix[0].length) {
+	        		matrix[i][j++]=0;
+	        	}
+            }
+         }   
+        }
+    }
     
     public static void printMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
