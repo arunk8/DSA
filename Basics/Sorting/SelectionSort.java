f77d249fbf2b170de05d1df0aa4bc8d9fad359e3
Arun Kumar <arunraj963686@gmail.com>
2023-12-03 18:27:05 +0530

    sorting program added

--- Basics/Sorting/SelectionSort.java
+++ Basics/Sorting/SelectionSort.java
@@ -1,5 +1,32 @@
 package Sorting;
 
 public class SelectionSort {
-
+	public static void main(String args[]) {
+		int arr[]= {12,56,1,32,21,54,9};
+		int n=arr.length;
+		System.out.println("Un-Sorted Array : ");
+		for(int i=0;i<n;i++) {
+			System.out.print(arr[i] +" ");
+		}
+		selection_sort(arr, n);
+	}
+	
+	public static void selection_sort(int arr[],int n) {
+		for(int i=0;i<n-1;i++) { 
+			int min=i;
+			for(int j=i+1;j<n;j++) {
+				if(arr[j]<arr[min]) {
+					min=j;
+				}
+			}
+			//swap
+			int temp= arr[min];
+			arr[min] = arr[i];
+			arr[i] = temp;
+		}
+		System.out.println("\nSorted Array : ");
+		for(int i=0;i<n;i++) {
+			System.out.print(arr[i] +" ");
+		}
+	}
 }
