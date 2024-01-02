31cab1b4b60825357f02276c05c4879c71f1fa52
Arun Kumar <arunraj963686@gmail.com>
2024-01-02 17:23:46 +0000

    Recursion package added

---
+++ Recursion/Power.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+import java.util.*;
+public class Power{
+public static double myPow(double x, int n) {
+       double ans = 1.0;
+       for(int i = 0; i<n; i++){
+           ans = ans * x;
+       }
+       return ans;
+   }
+   public static void main(String args[])
+   {
+       System.out.println(myPow(2,10));
+   }
+}
