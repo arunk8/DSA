a35ab02759088112ec41b98d6ad107eadb53317b
Arun Kumar <arunraj963686@gmail.com>
2024-01-10 14:43:01 +0000

    Arrays updated

--- Basics/Arrays/PascalsTriangle.java
+++ Basics/Arrays/PascalsTriangle.java
@@ -1,10 +1,30 @@
 package Arrays;
+import java.util.*;
 
 public class PascalsTriangle {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//validate pascals triangle
-	}
+    public static long nCr(int n, int r) {
+        long res = 1;
 
+        // calculating nCr:
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return res;
+    }
+
+    public static void pascalTriangle(int n) {
+        // printing the entire row n:
+        for (int c = 1; c <= n; c++) {
+            System.out.print(nCr(n - 1, c - 1) + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        pascalTriangle(n);
+    }
 }
+    
