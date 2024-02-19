a893a532c9d88926ffbca172675f5c49ffaf0e5a
Arun Kumar <arunraj963686@gmail.com>
2024-02-19 04:10:59 +0000

    sorting

--- Basics/Sorting/BubbleSort.java
+++ Basics/Sorting/BubbleSort.java
@@ -4,7 +4,7 @@ public class BubbleSort {
 	public static void main(String args[]) {
         int arr[] = {13, 46, 24, 52, 20, 9};
         int n = arr.length;
-        System.out.println("Before Using Bubble Sort: ");
+        System.out.println("Before Using Bubble Sort - ");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
